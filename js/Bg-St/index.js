ab4a8eb8bb9db5c79ecb5c6ac4f47b5eed1827e7
Makarov Ilya <makarov.i.1415@gmail.com>
2022-09-07 17:11:55 +0500

    fix: update index.js file

--- js/Bg-St/index.js
+++ js/Bg-St/index.js
@@ -1,26 +1,49 @@
-// const myName = {
-// 	firstName: 'Ilya',
-// 	lastName: 'Makarov'
-// }
-// const copyName = myName
-// const copyNameG = copyName
-// copyNameG.gender = 'Male'
-// const myNameFull = myName
-// myNameFull.date = 1999
-// myNameFull.lastName = 'Makovvv'
-// const sayMyName = () => {
-// 	console.log(myNameFull)
-// }
-
-// sayMyName()
-
+const name = 'Ilya'
+const age = 23
+const userProfile = {
+	name,
+	age,
+	lastName: 'Makov'
+}
+console.log(userProfile)
+// 
 const myCity = {
-	city: 'St. Petersburg',
-	info: {
-		isPopular: true,
-		country: 'Russia'
+	city: 'Ekb',
+	cityGreeting: function () {
+		console.log('Greetings')
 	}
 }
-console.log(myCity.info)
-delete myCity.info.isPopular
-console.log(myCity.info)
+myCity.cityGreeting()
+
+const person = {
+	name: 'Mak',
+	age: 23,
+	local: {
+		city: 'Chel'
+	}
+}
+const person2 = JSON.parse(JSON.stringify(person))
+person2.local.city = 'Ekb'
+console.log(person)
+console.log(person2)
+
+
+function myFn(a, b) {
+	let c
+	a = a + 1
+	c = a + b
+	return c
+}
+const sumMyFn = myFn(3, 3)
+console.log(sumMyFn)
+
+
+function multiply(a, b){
+	let c
+	c = a * b
+	return c
+}
+const sumMul = multiply(10, 5)
+console.log(sumMul)
+
+// console.log(multiply(10, 5))
