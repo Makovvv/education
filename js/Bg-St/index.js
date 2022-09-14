99a345bb49e98d0ebe8f2c6ec021add09cdf743f
Makarov Ilya <makarov.i.1415@gmail.com>
2022-09-14 21:35:54 +0500

    fix: update js file

--- js/Bg-St/index.js
+++ js/Bg-St/index.js
@@ -92,30 +92,87 @@
 
 // let a = 1
 // let b = 2
-const sumPositiveNumbers = (a, b) => {
-	if (typeof a !== 'number' || typeof b !== 'number') {
-		return 'One of the arguments in not a number'
-	}
-	if (a <= 0 || b <= 0) {
-		return 'Numbers are not positive'
-	}
-	return a + b
-}
-console.log(sumPositiveNumbers(66, 3))
 
-const month = 2
-switch (month) {
-	case 12:
-		console.log('Dec')
-		break
-	case 2:
-		console.log('Feb')
-		break
-	case 1:
-		console.log('Jan')
-		break
-	default:
-		console.log('Это не зимний месяц')
-}
+// const month = 2
+// switch (month) {
+// 	case 12:
+// 		console.log('Dec')
+// 		break
+// 	case 2:
+// 		console.log('Feb')
+// 		break
+// 	case 1:
+// 		console.log('Jan')
+// 		break
+// 	default:
+// 		console.log('Это не зимний месяц')
+// }
 
+// const sumPositiveNumbers = (a, b) => {
+// 	if (typeof a !== 'number' || typeof b !== 'number') {
+// 		return 'One of the arguments in not a number'
+// 	}
+// 	if (a <= 0 || b <= 0) {
+// 		return 'Numbers are not positive'
+// 	}
+// 	return a + b
+// }
+// console.log(sumPositiveNumbers(66, 3))
 
+// const value1 = 17
+// const value2 = 13
+// value1 && value2
+// 	? console.log(sumPositiveNumbers(value1, value2))
+// 	: console.log('One of the arguments in not a number')
+
+// const val = 10
+// val
+// 	? console.log('Условие истино')
+// 	: console.log('Условие ложно')
+
+// let value = -5
+// const res = value >= 0 ? value : -value
+// console.log(res) // 5
+
+// let i = 10
+// do {
+// 	console.log(i)
+// 	i++
+// } while(i<5)
+
+// const myObject = {
+// 	x: 10,
+// 	y: true,
+// 	z: 'abc'
+// }
+// for (const znak in myObject){
+// 	console.log(znak, myObject[znak])
+// }
+
+// const myString = 'hey'
+//  for (const letter of myString){
+// 	console.log(letter)
+// }
+
+// const myObject = {
+// 	x: 10,
+// 	y: true,
+// 	z: 'Mak'
+// }
+// Object.keys(myObject).forEach(key => {
+// 	console.log(key, myObject[key])
+// })
+
+// Object.values(myObject).forEach(value =>{
+// 	console.log(value)
+// })
+const one = 4
+import sayMyName from './moduleTwo.js'
+sayMyName() // Makarov
+
+import {
+	one as oneRenamed,
+	two
+} from './moduleTwo.js'
+console.log(oneRenamed) // 1
+console.log(two) // 6
